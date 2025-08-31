7738845abdfa432b0380b38548a70eee80185ac2
Roman <roman444uk@gmail.com>
2025-08-30 22:10:41 -0400

    Add `LaravelKit\Log\Filebeat\FormatterContextError`

---
+++ src/Log/Filebeat/FormatterContextError.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace LaravelKit\Log\Filebeat;
+
+class FormatterContextError implements FormatterContext
+{
+    public function __construct(
+        protected $message,
+        protected array $logData = [],
+        protected ?string $event = null,
+        protected ?string $triggered = null,
+    )
+    {
+    }
+
+    public function format(): array
+    {
+        return [
+            'exception' => [
+                'route' => request()?->route()?->uri,
+                'message' => $this->message,
+            ]
+        ];
+    }
+}
